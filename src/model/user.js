262ce2d8d567fe287799dccec27559eece176e52
hungvo2003vn <votanhung2003@gmail.com>
2024-02-23 21:35:17 +0700

    GDSC - test first query

--- src/model/user.js
+++ src/model/user.js
@@ -1,26 +1,17 @@
-import mongoose from 'mongoose'
-const Schema = mongoose.Schema
-const user = new Schema(
-	{
-		email: {type: String, default: null, maxLength: 100},
-		password: {type: String, default: null},
-		firstName: {type: String, default: null, maxLength: 50},
-		lastName: {type: String, default: null, maxLength: 50},
-		mssv: {type: String, default: null, maxLength: 50},
-		classes: {type: String, default: null, maxLength: 50},
-		major: {type: String, default: null, maxLength: 100},
-		role: {type: String, default: null, maxLength: 50},
-		balance: {type: Number, default: 0},
-		location: {
-			facility: {type: String, default: null, maxLength: 20},
-			department: {type: String, default: null, maxLength: 20},
-			room: {type: String, default: null, maxLength: 20},
-		},
-		printingLog: {type: [String]},
-		payment:{type: [String]},
-	},
-	{
-		timestamps: true,
-	}
-)
-export default mongoose.model('users', user)
+import db from '../config/firebase.js';
+
+async function getAllUsers() {
+    try {
+        const querySnapshot = await db.collection("User").get();
+        const users = [];
+        querySnapshot.forEach((doc) => {
+            users.push({ id: doc.id, ...doc.data() });
+        });
+        console.log(users);
+    } catch (error) {
+        console.error("Error getting users: ", error);
+        throw error;
+    }
+}
+
+export default getAllUsers;
